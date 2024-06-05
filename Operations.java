bef6515e3278b851cd3c5a1071742577ca4cc927
Allen Rodriguez <140763406+allen2rodriguez@users.noreply.github.com>
2024-06-05 12:26:28 -0700

    adding some more stuff

---
+++ Operations.java
@@ -0,0 +1,26 @@
+public class Operations {
+    
+    //add
+    public static add(double a, double b) {
+        return a + b;
+    }
+
+    //subtraact 
+    public static sub(double a, double b) {
+        return a - b;
+    }
+
+    //multiply 
+    public static multi(double a, double b) {
+        return a * b;
+    }
+
+    //divide
+    public static div(double a, double b) {
+        if (b == 0) {
+            return system.out.Println("Cannot divide by 0")
+        } else {
+            return a / b;
+        }
+    }
+}
