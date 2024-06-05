949d93a6fa3b2ff42efb36a2d878fcb0c1bf43a2
Allen Rodriguez <140763406+allen2rodriguez@users.noreply.github.com>
2024-06-05 12:26:37 -0700

    Adding in some new stuff

---
+++ testFunction.java
@@ -0,0 +1,9 @@
+public class testFunction {
+    public static void main(String[] args) {
+        Function newObject = new Function();
+        
+        double resultAdd = newObject.add(5,3);
+        double resultSub = newObject.sub(3,4);
+        double resultMulti = newObject.sub(5,3);
+    }
+}
