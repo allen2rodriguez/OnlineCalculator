4b19eca3b04ed9e71f64caf7e4ba858588ae8160
Allen Rodriguez <140763406+allen2rodriguez@users.noreply.github.com>
2024-06-05 12:32:44 -0700

    updating with the new name

--- testFunction.java
+++ testFunction.java
@@ -1,9 +1,10 @@
 public class testFunction {
     public static void main(String[] args) {
-        Function newObject = new Function();
+        Operations newObject = new Operations();
         
         double resultAdd = newObject.add(5,3);
         double resultSub = newObject.sub(3,4);
         double resultMulti = newObject.sub(5,3);
+        double restultDiv = newObject.div(55.2,3.3);
     }
 }
